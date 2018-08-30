229d6f537a26a67063dcd2fbbc5a38169d9fa1b7
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-08-30 11:26:38 +0300

    Removed Main.storyboard and ViewController and add LoginViewController.storyboard,-.swift Added UI to Login-.storyboard

---
+++ PhotoApp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  PhotoApp
+//
+//  Created by mac-089-71 on 8/30/18.
+//  Copyright © 2018 VPavelDm. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+
+}
