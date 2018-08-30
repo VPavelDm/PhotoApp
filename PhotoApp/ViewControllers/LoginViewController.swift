5f1bca094ae120bbf0fdf0fb5bd09c61e707b496
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-08-30 12:01:36 +0300

    Added error handling when a user tries to log in

--- PhotoApp/ViewControllers/LoginViewController.swift
+++ PhotoApp/ViewControllers/LoginViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginTF: UITextField!
-    @IBOutlet weak var PasswordTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
     
     @IBAction func clickLogin(_ sender: UIButton) {
-        
+        Auth.auth().signIn(withEmail: loginTF.text ?? "", password: passwordTF.text ?? "") { (authDataResult, error) in
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(action)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
     
 }
