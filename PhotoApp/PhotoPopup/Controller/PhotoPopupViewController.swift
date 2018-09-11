d54c187db3058d1f13c3462f23e0e1cb2d361703
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-09-11 17:03:50 +0300

    Fixed bug with not weak delegate

--- PhotoApp/PhotoPopup/Controller/PhotoPopupViewController.swift
+++ PhotoApp/PhotoPopup/Controller/PhotoPopupViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class PhotoPopupViewController: ViewController {
     
     var photo: Photo!
-    var delegate: PhotoPopupDelegate?
+    weak var delegate: PhotoPopupDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- PhotoApp/PhotoPopup/Delegate/PhotoPopupDelegate.swift
+++ PhotoApp/PhotoPopup/Delegate/PhotoPopupDelegate.swift
@@ -9,6 +9,6 @@
 import Foundation
 import UIKit
 
-protocol PhotoPopupDelegate {
+protocol PhotoPopupDelegate: NSObjectProtocol {
     func savePhoto(photo: Photo)
 }
