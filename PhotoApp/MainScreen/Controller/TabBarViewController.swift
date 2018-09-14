45bc0eeb4e7a9b150005ce43022100ad96d8d94b
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-09-14 12:50:53 +0300

    Fixed bug with observing updates of cloud

--- PhotoApp/MainScreen/Controller/TabBarViewController.swift
+++ PhotoApp/MainScreen/Controller/TabBarViewController.swift
@@ -9,38 +9,34 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
+    
+    private let map = NSLocalizedString("Map", comment: "Map label")
+    private let timeline = NSLocalizedString("Timeline", comment: "Timeline label")
+    private let more = NSLocalizedString("More", comment: "More label")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let mapViewController = MapViewController.create(asClass: MapViewController.self)
-        mapViewController.tabBarItem = UITabBarItem(title: NSLocalizedString("Map", comment: "Map label"), image: UIImage(named: "ic_map"), tag: 1)
+        mapViewController.tabBarItem = UITabBarItem(title: map, image: UIImage(named: "ic_map"), tag: 1)
         
         let timelineViewController = TimelineViewController.create(asClass: TimelineViewController.self)
-        timelineViewController.tabBarItem = UITabBarItem(title: NSLocalizedString("Timeline", comment: "Timeline label"), image: UIImage(named: "ic_timeline"), tag: 2)
+        timelineViewController.tabBarItem = UITabBarItem(title: timeline, image: UIImage(named: "ic_timeline"), tag: 2)
         let navigationViewController = UINavigationController(rootViewController: timelineViewController)
         
         let moreViewController = MoreViewController.create(asClass: MoreViewController.self)
-        moreViewController.tabBarItem = UITabBarItem(title: NSLocalizedString("More", comment: "More label"), image: UIImage(named: "ic_more"), tag: 3)
+        moreViewController.tabBarItem = UITabBarItem(title: more, image: UIImage(named: "ic_more"), tag: 3)
         
         viewControllers = [mapViewController, navigationViewController, moreViewController]
     }
     
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        for viewController in viewControllers! {
-            if let navigationController = viewController as? UINavigationController {
-                for viewController in navigationController.viewControllers {
-                    if let timelineViewController = viewController as? TimelineViewController {
-                        if timelineViewController.tabBarItem.title == item.title {
-                            for viewController in viewControllers! {
-                                if let mapViewController = viewController as? MapViewController {
-                                    timelineViewController.categories = mapViewController.categories
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        let mapViewController = viewControllers?.first as! MapViewController
+        let timelineViewController = (viewControllers![1] as! UINavigationController).viewControllers.first as? TimelineViewController
+        if item.title == map {
+            mapViewController.categories = timelineViewController!.categories
+        } else {
+            timelineViewController!.categories = mapViewController.categories
         }
     }
 
