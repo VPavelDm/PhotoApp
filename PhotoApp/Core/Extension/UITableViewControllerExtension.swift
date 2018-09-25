cdd794d122fc7f3ae2e8986d45cd9d4d7a9aaad2
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-09-25 17:58:43 +0300

    Removed useless method

--- PhotoApp/Core/Extension/UITableViewControllerExtension.swift
+++ PhotoApp/Core/Extension/UITableViewControllerExtension.swift
@@ -12,13 +12,5 @@ import UIKit
 extension UITableViewController {
     static func create<Controller>(storyboardId: String? = nil, asClass: Controller.Type) -> Controller {
         return ViewController.create(storyboardId: storyboardId, asClass: asClass)
-    }
-    
-    func showAlertWithError(message: String) {
-        let alert = UIAlertController(title: "Error".localized(), message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok".localized(), style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-    
+    }    
 }
